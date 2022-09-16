da01b07cfb08a936a1413bb2b02b5c4a1dcffde1
Daniel Oh <cabbachew@gmail.com>
2022-09-16 16:39:59 +0900

    Solve 11b_cat_spec.rb

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:jinx) { described_class.new('Jinx', 'Abyssinian', 'tawny')}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat shares method name with other classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(jinx).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(jinx).to be_hiding
   end
 end
